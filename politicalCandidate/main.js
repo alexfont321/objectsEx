5e579d7ed56079780e2bc1d29c35c62fcf5025f9
alexfont <alexfont321@gmail.com>
2018-07-16 10:48:05 -0500

    first commit for the objects ex

---
+++ politicalCandidate/main.js
@@ -0,0 +1,107 @@
+const elizabethSingler = {
+    district: "Tennessee-5th-District",
+    platform: {
+        taxes: "tax the upper classes",
+        jobs: "programs for people to change careers",
+        infrastructure: "new paved roads and bus system",
+        HealthCare: "affordable healthcare for lower class",
+        Crime: "give convicts chance to reform"
+    },
+    biography: "",
+    missionStatement: "Make Stuff Better"
+
+}
+
+const urls = {
+    donationForm: "www.nashvillesoftwareschool.com",
+    registerVote: "www.newyorktimes.com"
+}
+
+const eventsCalendar = {
+    april: "rally",
+    june: "roundtable with people in district",
+    july: {
+        firstSunday: "debate with bad guy",
+        thirdSunday: "kiss a baby"
+    }
+}
+
+const volunteerInfo = {
+    name: "",
+    address: "",
+    email: "",
+    phoneNumber: "",
+    availability: "",
+    activities: []
+}
+
+// changing the new bio
+
+let newBio = "I love baking!!"
+
+function changeBiography (bio) {
+    elizabethSingler.biography = bio;
+    console.log(bio)
+}
+
+changeBiography(newBio)
+
+console.log(elizabethSingler.biography)
+
+// adding volunteer info
+
+const myVolunteerInfo = {
+    name: "alejandro",
+    address: "nashville",
+    email: "politician@aol.com",
+    availability: "Wednesdays",
+    activities: ["calling", "canvasing", "picketing"],
+    phoneNumber: "2105555555"
+}
+
+function addVolunteerInfo (object) {
+    volunteerInfo.name = object.name;
+    volunteerInfo.address += object.address;
+    volunteerInfo.email += object.email;
+    volunteerInfo.availability += object.availability;
+    volunteerInfo.phoneNumber += object.phoneNumber;
+    // volunteerInfo.activities.push(object.activities);
+    for (let i = 0; i < object.activities.length; i++) {
+    volunteerInfo.activities.push(object.activities[i]);
+    }
+    // console.log(object);
+}
+
+addVolunteerInfo(myVolunteerInfo);
+
+console.log(volunteerInfo);
+
+
+// Changing the calendar 
+
+const newThirdSunday = "Spending time with Family"
+
+function changingThirdSunday (string) {
+    eventsCalendar.july.thirdSunday = string;
+}
+
+changingThirdSunday(newThirdSunday)
+
+console.log(eventsCalendar.july.thirdSunday)
+
+
+//// advanced Challenge 
+
+const createArticle = document.createElement("article");
+createArticle.id = elizabethSingler.district;
+
+console.log(createArticle)
+
+let targetID = document.getElementById('Tennessee-5th-District');
+
+
+targetID.appendChild = "<p>hey gang</p>";
+
+const bodyElement = document.querySelector("body");
+
+
